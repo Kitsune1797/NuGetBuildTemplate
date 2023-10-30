6627104f5280afa3ce97182cc10067c35668a0d0
Max Teng <72878257+Kitsune1797@users.noreply.github.com>
2023-10-30 16:30:38 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace NuGetBuildProject
+{
+    public class Class1
+    {
+        public string GetMessage()
+        {
+            return "Hello from your class library!";
+        }
+    }
+}
